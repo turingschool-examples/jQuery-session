8b88908d1f70d3e60714f2ffaf7347845b7d1942
Chris Postma <cjrpostma@gmail.com>
2020-02-13 10:15:05 -0700

    Add selectors

--- part-1-selectors/selectors.js
+++ part-1-selectors/selectors.js
@@ -1,11 +1,24 @@
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(e){
-  e.preventDefault();
-})
+$(document).ready(function() {
+  const checkedBoxId = $('input[type="checkbox"]:checked').attr("id");
+  console.log(checkedBoxId);
 
-let clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', function(){
-  document.querySelectorAll('input').forEach(input => {
-    input.value = "";
-  })
-})
+  const btn = document.querySelector("button");
+  btn.addEventListener("click", function(e) {
+    e.preventDefault();
+  });
+
+  const clearBtn = document.querySelector("#clear-btn");
+  clearBtn.addEventListener("click", function() {
+    document.querySelectorAll("input").forEach(input => {
+      input.value = "";
+    });
+  });
+
+  $("label").addClass("green");
+  $(".phone").addClass("yellow");
+  $("label[for='email']")
+    .removeClass("green")
+    .addClass("blue");
+  $("p label").addClass("purple");
+  $("#clear-btn").addClass("red");
+});
