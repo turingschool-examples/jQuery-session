3649c50d1c52bdd4b3a5bad832c81a1ddb2f14aa
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-13 09:30:31 -0700

    Add initial questions and do first exercise

---
+++ intro-exercise/intro.js
@@ -0,0 +1,6 @@
+// 2. In the `intro.js` file, use jQuery to select the span element and:
+//   1. change the span's text to say '1911'
+//   2. give the span a class of 'green'
+// 3. Commit your changes!
+
+$('span').text('1911').addClass('green');
