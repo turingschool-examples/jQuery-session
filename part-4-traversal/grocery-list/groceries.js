e334e15d0c19279dfbedbafa51b552a4f8461f7d
Cristina Pena <cristinalpena@gmail.com>
2020-02-13 13:53:37 -0700

    Finish groceries

--- part-4-traversal/grocery-list/groceries.js
+++ part-4-traversal/grocery-list/groceries.js
@@ -1,5 +1,20 @@
-const $ = require('jquery');
-
 $('.cheese').click(function() {
 	$('.cheeses').children().css('background-color', 'pink')
 })
+
+$('.tomato-sauce').parent().css('background-color', 'red')
+
+$('.lettuce').click(function(){
+	$(this).siblings().css('background-color', 'green').css('color', 'white');
+})
+
+$('li').click(function(){
+	$(this).toggleClass('strikeout');
+})
+
+$('.pizza-ingredients').children('.cheeses').css("font-weight","Bold");
+
+
+$('.groceries').children().first().css('color', 'blue');
+
+$('.groceries').children().css('background-color', 'lightgrey').not('.dough');
