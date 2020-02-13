06f4551075c032de2f258c1a3016069d201e0463
Edward Stoner <edwardlstoner@gmail.com>
2020-02-13 11:11:20 -0700

    Complete exercises

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -1,14 +1,34 @@
-const $ = require('jquery');
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(e){
-  e.preventDefault();
+// const $ = require('jquery');
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(e){
+//   e.preventDefault();
+// })
+
+let btn2 = $("[type='submit']")
+btn2.on('click', function(e){
+  e.preventDefault()
 })
 
-let clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', function(){
-  document.querySelectorAll('input').forEach(input => {
-    input.value = "";
-  })
+let clearBtn = $('#clear-btn');
+clearBtn.on('click', function() {
+  $('input').val('')
 })
+
+$('input').on('input', function(e) {
+  localStorage.setItem('info', $(e.target).val())
+})
+
+$('input').val(localStorage.getItem('info'))
+
+$('#clear-btn').on('click', function(e) {
+  localStorage.clear()
+})
+
+// let clearBtn = document.querySelector('#clear-btn');
+// clearBtn.addEventListener('click', function(){
+//   document.querySelectorAll('input').forEach(input => {
+//     input.value = "";
+//   })
+// })
 
 console.log($('p').text())
