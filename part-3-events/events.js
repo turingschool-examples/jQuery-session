3f15ce7bbfdb768c43a13fe6e25721d60e376d0a
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-02-13 15:01:00 -0700

    Complete part-3

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -1,14 +1,34 @@
-const $ = require('jquery');
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(e){
-  e.preventDefault();
-})
+// const $ = require('jquery');
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(e){
+//   e.preventDefault();
+// })
 
-let clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', function(){
-  document.querySelectorAll('input').forEach(input => {
-    input.value = "";
+// let clearBtn = document.querySelector('#clear-btn');
+// clearBtn.addEventListener('click', function(){
+//   document.querySelectorAll('input').forEach(input => {
+//     input.value = "";
+//   })
+// })
+
+$(document).ready(function() {
+  $('input').each(function() {
+    $(this).val(localStorage.getItem($(this).attr('id')));
   })
+
+  $('button').click(function() {
+    event.preventDefault()
+  })
+
+  $('#clear-btn').on("click", function() {
+    $("input").val('');
+    localStorage.clear()
+  })
+
+  $("input").on('keyup', function(event) {
+    localStorage.setItem(event.target.id, $(event.target).val())
+  })
+
 })
 
 console.log($('p').text())
