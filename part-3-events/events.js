a184d737222118ac04d8b9abbab2f7b9107027f8
Olivia Webster <oliviacweb@gmail.com>
2020-02-13 11:39:19 -0700

    Add part 3 events

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -1,14 +1,38 @@
-const $ = require('jquery');
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(e){
+// const $ = require('jquery');
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(e){
+//   e.preventDefault();
+// })
+
+// let clearBtn = document.querySelector('#clear-btn');
+// clearBtn.addEventListener('click', function(){
+//   document.querySelectorAll('input').forEach(input => {
+//     input.value = "";
+//   })
+// })
+
+// let btn = $('button');
+
+$('#clear-btn').click(function() {
+  $('input').val('');
+  localStorage.clear();
+})
+
+$('button').click(function(e) {
   e.preventDefault();
 })
 
-let clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', function(){
-  document.querySelectorAll('input').forEach(input => {
-    input.value = "";
-  })
+$('input').on('input', function() {
+  localStorage.setItem('name', $('#name').val());
+  localStorage.setItem('email', $('#email').val());
+  // localStorage.setItem('number', JSON.stringify($('.phone').map(val())));
+  //couldnt get phone numbers
+});
+
+$(document).ready(function() {
+  $('#name').val(localStorage.getItem('name'));
+  $('#email').val(localStorage.getItem('email'));
 })
+
 
 console.log($('p').text())
