b39166fc710bf6b1ee54ccb136b0defd09b452d3
Cristina Pena <cristinalpena@gmail.com>
2020-02-13 12:58:51 -0700

    Finish part 3

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -1,27 +1,25 @@
-// const $ = require('jquery');
-// let btn = document.querySelector('button');
-// btn.addEventListener('click', function(e){
-//   e.preventDefault();
-// })
-
 $("button").on("click", function(e){
   e.preventDefault();
 });
 
-// let clearBtn = document.querySelector('#clear-btn');
-// clearBtn.addEventListener('click', function(){
-//   document.querySelectorAll('input').forEach(input => {
-//     input.value = "";
-//   })
-// })
-
 $("#clear-btn").click(function(){
   $("input").val("");
+  localStorage.clear();
 })
 
 console.log($('p').text())
 
-$("input").on("change", function())
-// 2. Listen for an `input` event on all inputs on the page. When the event fires, save that input's value to local storage
-// 3. When the page loads, set the value of the inputs to what was saved in local storage
-// 4. When you click the clear button, clear local storage as well.
+$("input").on("keyup", function(){
+  let inputKey = $(this).attr("id");
+  localStorage.setItem(inputKey, JSON.stringify($(this).val()));
+})
+
+$(document).ready(function() {
+  $("input").each(function(){
+    let inputKey = $(this).attr("id");
+    if(localStorage.hasOwnProperty(inputKey)) {
+      let savedInput = JSON.parse(localStorage.getItem(inputKey));
+      $(this).val(savedInput);
+    }
+  })
+});
