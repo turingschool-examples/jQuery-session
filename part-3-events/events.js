3d12eb7fbabba8c9cf04ba6810184e02f83069f3
Jeremy Poulter <Jpoulter@gmail.com>
2020-02-13 15:09:57 -0700

    complete the part-3-events questions and exercises

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -1,14 +1,40 @@
-const $ = require('jquery');
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(e){
-  e.preventDefault();
-})
+// const $ = require('jquery');
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(e){
+//   e.preventDefault();
+// })
 
-let clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', function(){
-  document.querySelectorAll('input').forEach(input => {
-    input.value = "";
-  })
-})
+
+// let clearBtn = document.querySelector('#clear-btn');
+// clearBtn.addEventListener('click', function(){
+//   document.querySelectorAll('input').forEach(input => {
+//     input.value = "";
+//   })
+// })
+
+$('')
+
+$(document).ready(() => {
+
+  $('input').each(function () {
+    $(this).val(localStorage.getItem($(this).attr('id')));
+  });
+
+
+  $('button').click(() => {
+    event.preventDefault();
+  });
+
+  $('#clear-btn').on('click', () => {
+    $('input').val('');
+    localStorage.clear();
+  });
+
+  $('input').on('input', (event) => {
+    localStorage.setItem(event.target.id, $(event.target).val());
+  });
+});
+
+
 
 console.log($('p').text())
