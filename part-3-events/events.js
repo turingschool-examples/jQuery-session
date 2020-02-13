8ea0cbf4e5fd0100c2e3cbe40f2d4f01599e24a0
Chris Postma <cjrpostma@gmail.com>
2020-02-13 12:29:56 -0700

    Add events

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -16,15 +16,29 @@ $(document).ready(function() {
 
   // 2. Listen for an`input` event on all inputs on the page. When the event fires, save that input's value to local storage
   $("input").on("input", function() {
-    const key = $(this).attr("id");
-    const value = $(this).val();
+    const cachedElement = $(this);
+    const key = cachedElement.attr("id");
+    let value = cachedElement.val();
+    const isCheckbox = cachedElement.is('input[type="checkbox"]');
+
+    if (isCheckbox) {
+      const isChecked = cachedElement.is(":checked");
+      value = isChecked;
+    }
+
     localStorage.setItem(key, value);
   });
+
   // 3. When the page loads, set the value of the inputs to what was saved in local storage
   $("input").each(function() {
-    const key = $(this).attr("id");
+    const cachedElement = $(this);
+    const key = cachedElement.attr("id");
     const value = localStorage.getItem(key);
-    if (key) {
+    const isCheckbox = cachedElement.is('input[type="checkbox"]');
+
+    if (key && isCheckbox) {
+      cachedElement.prop("checked", value);
+    } else if (key) {
       $(this).val(value);
     }
   });
