2e9d84e7a02626d70532877e7687766e5a76d353
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-13 12:40:40 -0700

    Made changes to html based on exercise spec

--- part-2-content/content.js
+++ part-2-content/content.js
@@ -1,4 +1,4 @@
-const $ = require('jquery');
+// const $ = require('jquery');
 let btn = document.querySelector('button');
 btn.addEventListener('click', function(e){
   e.preventDefault();
@@ -12,3 +12,14 @@ clearBtn.addEventListener('click', function(){
 })
 
 console.log($('p').text())
+// check to make sure I'm getting the right element
+const yolo = $('input').val();
+console.log(yolo);
+
+$('button[type=submit]').text('Add Contact');
+$('label[for=confirm]').text('Yes!!!');
+$('label[for=deny]').text('No!!!');
+$('input').val('Enter something here');
+$('input[class=phone]').val('555');
+$('input[class=phone]:last-of-type').val('5555');
+$('h1, h2').text('Contact Form');
